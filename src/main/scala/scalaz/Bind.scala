937f006ed920349818eb9295e07573f3758679c2
xuwei-k <6b656e6a69@gmail.com>
2016-01-23 07:16:47 +0900

    Bind#ap

--- src/main/scala/scalaz/Bind.scala
+++ src/main/scala/scalaz/Bind.scala
@@ -14,6 +14,11 @@ trait Bind[F[_]] extends ExtendsBind[F] { self: Apply[F] =>
   override def apply: Apply[F] = this
   ////
   def bind[A, B](fa: F[A])(f: A => F[B]): F[B]
+
+  override def ap[A, B](fa: => F[A])(f: => F[A => B]): F[B] = {
+    lazy val fa0 = fa
+    bind(f)(functor.map(fa0))
+  }
   ////
 }
 
